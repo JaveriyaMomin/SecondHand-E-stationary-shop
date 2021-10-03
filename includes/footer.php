dd9cb02bd87c96bb594b22c666767fc01d8bc2ee
techlifess <74123364+techlifess@users.noreply.github.com>
2021-10-03 17:04:09 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,44 @@
+<div id="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 col-sm-6">
+				<h4>Pages</h4>
+				<ul>
+					<li><a href="checkout.php">My Account</a></li>
+					<li><a href="shop.php">Shop</a></li>
+					<li><a href="cart.php">Shopping Cart</a></li>
+					<li><a href="contactus.php">Contact Us</a></li>
+				</ul>
+				<hr>
+				<h4>User Section</h4>
+				<ul>
+					<li><a href="checkout.php">Login</a></li>
+					<li><a href="customer_registration.php">Registration</a></li>
+				</ul>
+				<hr class="hidden-md hidden-lg hidden-sm">
+			</div>
+			<div class="col-md-3 col-sm-6">
+				<h4>Product Categories</h4>
+				<ul>
+					<li><a href="#">Books</a></li>
+					<li><a href="#">Materials</a></li>
+				</ul>
+				<hr class="hidden-md hidden-lg">
+			</div>
+			<!--<div id="copyright">
+				<div class="container">
+					<div class="col-md-6">
+						<p class="pull-left">
+							&copy Juveriya, Ayaan, Maaz, Faizan
+						</p>
+					</div>
+					<div class="col-md-6">
+						<p class="pull-right">
+							Student of: Rizvi College Of Engineering
+						</p>
+					</div>
+				</div>
+			</div>-->
+		</div>
+	</div>
+</div>
---
+++ includes/sidebar.php
@@ -0,0 +1,28 @@
+<div class="panel panel-default sidebar-menu">
+	<div class="panel-heading">
+		<h3 class="panel-title">Product Categories</h3>
+		
+	</div>
+	<div class="panel-body">
+		<ul class="nav nav-pills nav-stacked category-menu">
+			<li><a href="shop.php">Books</a></li>
+			<li><a href="shop.php">Materials</a></li>
+		</ul>
+	</div>
+	
+</div>
+<div class="panel panel-default sidebar-menu">
+	<div class="panel-heading">
+		<h3 class="panel-title">Class</h3>
+		
+	</div>
+	<div class="panel-body">
+		<ul class="nav nav-pills nav-stacked category-menu">
+			<li><a href="shop.php">FE</a></li>
+			<li><a href="shop.php">SE</a></li>
+			<li><a href="shop.php">TE</a></li>
+			<li><a href="shop.php">BE</a></li>
+		</ul>
+	</div>
+	
+</div>
